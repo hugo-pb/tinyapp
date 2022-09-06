3c71f64c0bf1cf8551d01de7d71bd870f429c766
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-05 20:29:39 -0600

    added back endo for showing one url

--- express_server.js
+++ express_server.js
@@ -20,6 +20,13 @@ app.get("/urls", (req, res) => {
   const templateData = { urls: urlDatabase };
   res.render("urls_index", templateData);
 });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
